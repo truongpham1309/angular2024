0f5b630079c8d028a4bbcc33ff1ce3dfe8c4f04a
truongpham1309 <truong13092002@gmail.com>
2024-01-03 10:06:09 +0700

    day1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../app/components/header/header.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-angular';
-  name = "Pham Dinh Truong";
+  name: string = "Pham Dinh Truong";
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "/products",
+        component: ProductsComponent
+    }
+];
